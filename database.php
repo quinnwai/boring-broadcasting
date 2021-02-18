5eec8ea7c3b5489c033b3e7dded2af39a89cd430
Vishal <agarva51@gmail.com>
2021-02-18 13:52:48 -0600

    database working

--- database.php
+++ database.php
@@ -1,8 +1,7 @@
 <?php
 // Content of database.php
 
-$mysqli = new mysqli('qwong@ec2-18-219-113-131.us-east-2.compute.amazonaws.com'
-, 'news_admin', 'news_pass', 'news');
+$mysqli = new mysqli('localhost', 'news_admin', 'news_pass', 'news');
 
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
