18c2a32b6782eefec0f9e489f42205949608dcf4
quinnwaiwong <quinnwai.wong@gmail.com>
2021-02-19 23:20:43 -0600

    authenticate with debug print statements

--- authenticate.php
+++ authenticate.php
@@ -9,7 +9,12 @@ $is_user = false;
 
 //read in login info
 $username = strtolower((string) $_POST['username']);
-$hash = password_hash((string) $_POST['password']);
+// $hash = password_hash((string) $_POST['password']);
+$password = (string) $_POST['password'];
+$hash = password_hash($password, PASSWORD_DEFAULT);
+printf("username is %s \n", htmlentities($username));
+printf("password is %s \n", htmlentities($password));
+printf("hash is %s \n <br><br>", $hash);
 
 //// 1: Validate Username ////
 //read from the users table
@@ -24,8 +29,12 @@ $stmt->bind_result($username_db, $hash_db);
 
 //for each username (row) in db, check if username already exists
 while($stmt->fetch()){
-    if ($username == strtolower($username_db) && $hash == $hash_db){
+    printf("comparing user %s to %s which is %d \n", htmlentities($username), htmlentities($username_db), htmlentities($username == strtolower($username_db)));
+    printf("comparing hash %s to %s which is %d \n ", $hash, $hash_db, htmlentities($hash == $hash_db));
+    printf("total truth value is %d \n <br><br>", htmlentities($username == strtolower($username_db) && $hash == $hash_db));
+    if ($username == strtolower($username_db) && strcmp($hash, $hash_db) == 0){
         $is_user = true;
+        printf("setting isUser to true!!");
         break;
     }
 }; 
