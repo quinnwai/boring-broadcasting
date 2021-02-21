d24955a491509e0f8d2b34cc21e2e6eafb27bc1f
Vishal <agarva51@gmail.com>
2021-02-20 22:27:10 -0600

    commented out useless file

--- user_data.php
+++ user_data.php
@@ -1,36 +1,36 @@
-<!-- <?php
-require "database.php";
+<?php
+// require "database.php";
 
-$username = $_SESSION["username"];
+// $username = $_SESSION["username"];
 
-$story_stmt = $mysqli->prepare("SELECT `id`, `username`, `title`, `body`, `link`,
-FROM `stories` WHERE username=$username");
-if(!$stmt){
-	printf("Query Prep Failed: %s\n", $mysqli->error);
-	exit;
-}
+// $story_stmt = $mysqli->prepare("SELECT `id`, `username`, `title`, `body`, `link`,
+// FROM `stories` WHERE username=$username");
+// if(!$stmt){
+// 	printf("Query Prep Failed: %s\n", $mysqli->error);
+// 	exit;
+// }
 
-$stmt->execute();
+// $stmt->execute();
 
-$stmt->bind_result($id, $title, $body, $link, $comments, $post_date);
+// $stmt->bind_result($id, $title, $body, $link, $comments, $post_date);
 
-echo "<ul>\n";
-while($stmt->fetch()){
-	printf("\t<li>Title: %s <br> 
-	Body: %s <br> 
-	Link: %s <br>
-    <ul>\n
-	\t<li>Comment : %s <br>
-	Post Date: %s",
-		htmlspecialchars($title),
-		htmlspecialchars($body),
-        htmlspecialchars($link),
-		htmlspecialchars($comments),
-		htmlspecialchars($post_date)
-	); 
+// echo "<ul>\n";
+// while($stmt->fetch()){
+// 	printf("\t<li>Title: %s <br> 
+// 	Body: %s <br> 
+// 	Link: %s <br>
+//     <ul>\n
+// 	\t<li>Comment : %s <br>
+// 	Post Date: %s",
+// 		htmlspecialchars($title),
+// 		htmlspecialchars($body),
+//         htmlspecialchars($link),
+// 		htmlspecialchars($comments),
+// 		htmlspecialchars($post_date)
+// 	); 
     
     ?>
-    <form action ="delete_comment.php" method="POST">
+   <!--  <form action ="delete_comment.php" method="POST">
         <input type="hidden" name="filename" value="<?php printf($files[$i]); ?>"/>
         <input type="submit" value ="delete"/>
     </form>
@@ -48,11 +48,11 @@ while($stmt->fetch()){
     <form action ="edit_story.php" method="POST">
         <input type="hidden" name="filename" value="<?php printf($files[$i]); ?>"/>
         <input type="submit" value ="delete"/>
-    </form>
-    <?php echo "</li>" ?>
-	<?php 
-}
-echo "</ul>\n";
-$stmt->close();
+    </form> -->
+    <?php 
+//     echo "</li>";
+// }
+// echo "</ul>\n";
+// $stmt->close();
 
-?> -->
+?> 
