a1ab0287ed8bac86aedf51797fdefd12210a93d2
Vishal <agarva51@gmail.com>
2021-02-21 22:45:09 -0600

    add user details

--- user_details.php
+++ user_details.php
@@ -7,8 +7,7 @@ require 'get_token.php';
 //get details to update
 $user = (string)$_SESSION['user'];
 
-$stmt = $mysqli->prepare("SELECT last_name, first_name
-FROM users WHERE username=?");
+$stmt = $mysqli->prepare("SELECT last_name, first_name FROM users WHERE username=?");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -34,6 +33,59 @@ while($stmt->fetch()){
 echo "</ul>\n";
 
 $stmt->close();
+
+$stmt2 = $mysqli->prepare("SELECT id, title, body, link FROM stories WHERE username = ?");
+if(!$stmt2){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt2->bind_param('s', $user);
+
+$stmt2->execute();
+
+$stmt2->bind_result($id, $title, $body, $link);
+
+?>
+<form action="logout.php">
+Logout and return to sign in page? <input type="submit" value = "Logout"/>
+</form>
+
+<?php
+echo "<ul>\n";
+while($stmt2->fetch()){
+	printf("\t
+	<li>Title: %s</a><br>
+	Body: %s<br>
+	</li>\n",
+		htmlspecialchars($title),
+		htmlspecialchars($body)
+	); 
+	?>
+	<div>
+	<form action ="view_story.php" method="POST">
+        <input type="hidden" name="story_id" value="<?php printf($id); ?>"/>
+        <input type="hidden" name="token" value="<?php echo $_SESSION['token'];?>" />
+		<input type="submit" name ="view_story" value = "view"/>
+    </form>
+	<form action ="delete_story.php" method="POST">
+        <input type="hidden" name="story_id" value="<?php printf($id); ?>"/>
+		<input type="hidden" name="token" value="<?php echo $_SESSION['token'];?>" />
+        <input type="submit" name ="delete_story" value = "delete"/>
+    </form>
+    <form action="edit_story_form.php" method="POST">
+        <input type="hidden" name="story_id" value="<?php printf($id); ?>"/>
+		<input type="hidden" name="title" value="<?php printf($title); ?>"/>
+		<input type="hidden" name="body" value="<?php printf($body); ?>"/>
+		<input type="hidden" name="link" value="<?php printf($link); ?>"/>
+		<input type="hidden" name="token" value="<?php echo $_SESSION['token'];?>" />
+        <input type="submit" name ="edit_story" value = "edit"/>
+    </form>
+	</div>
+	<?php 
+}
+echo "</ul>\n";
+$stmt2->close();
 
 ?>
 
