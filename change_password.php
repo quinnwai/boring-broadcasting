97ea3f7b97b69fa0b8c35f7d2d156603acfe779a
Vishal <agarva51@gmail.com>
2021-02-21 20:13:14 -0600

    change password stuff done

--- change_password.php
+++ change_password.php
@@ -3,44 +3,18 @@ session_start();
 require 'database.php';
 require 'get_token.php';
 
-$old_pwd = (string)$_POST['old_password'];
-$new_pwd = (string)$_POST['new_password'];
+$pwd = (string)$_POST['pwd'];
 $user = $_SESSION['user'];
 
-$stmt = $mysqli->prepare("SELECT password FROM users WHERE username = $user");
-
-$stmt->execute();
-
-// Bind the results
-$stmt->bind_result($pwd_hash);
-$stmt->fetch();
-
+$stmt = $mysqli->prepare("UPDATE users SET password= ? WHERE username = ?");
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
 }
-
-if (password_verify($old_pwd, $pwd_hash)) {
-    $stmt2 = $mysqli->prepare("UPDATE `users` SET 
-    `password`= $new_pwd WHERE username = $user");
-if(!$stmt2){
-	printf("Query Prep Failed: %s\n", $mysqli->error);
-	exit;
-}
-
-$stmt2->execute();
-$stmt2->close();
-echo "Success your password has been successfully changed";
-}
-else {
-    echo "Sorry your old password did not match the original password. Please resubmit";
-}
-
-// $stmt->bind_param('s', $story_id);
-
+$stmt->bind_param('ss', password_hash($pwd, PASSWORD_DEFAULT), $username);
 $stmt->execute();
-
 $stmt->close();
+echo "Success your password has been successfully changed";
 
 // TODO: if this doesn't work, use form because of CSRF stuff and include hidden var as seen in feed
 ?>
--- user_details.php
+++ user_details.php
@@ -5,31 +5,41 @@ require 'database.php';
 require 'get_token.php';
 
 //get details to update
-$user = $_SESSION['user'];
+$user = (string)$_SESSION['user'];
 
-$stmt = $mysqli->prepare("SELECT last_name, first_name, password
-FROM users WHERE username = ?");
-
-$stmt->bind_param('s', $user);
+$stmt = $mysqli->prepare("SELECT last_name, first_name
+FROM users WHERE username=?");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
 }
 
+$stmt->bind_param('s', $user);
+
 $stmt->execute();
-$stmt->bind_result($last, $first, $password);
+$stmt->bind_result($last, $first);
+
+
+echo "Welcome " . $user . "<br>";
+echo "Please find below your name according to our records: ";
+
+echo "<ul>\n";
+while($stmt->fetch()){
+	printf("\t<li>%s %s</li>\n",
+		htmlspecialchars($first),
+		htmlspecialchars($last)
+	);
+}
+echo "</ul>\n";
 
 $stmt->close();
 
-echo "Welcome " . $user . "<br>";
-echo "Your full name according to our records is " . $first . " " . $last;
 ?>
 
 <p> Want to change your password? </p>
 <form action="change_password.php" method="POST">
 <input type="hidden" name="token" value="<?php echo $_SESSION['token'];?>" />
-<label>Old Password: <input type="password" name="old_password"/> </label>
-<label>New Password: <input type="password" name="new_password"/> </label>
+<label>New Password: <input type="password" name="pwd"/> </label>
 <input type="submit" value="return" />
 
