8fbe4f1aef4c3a219d6d0c839485c77a28b58a15
quinnwaiwong <quinnwai.wong@gmail.com>
2021-02-22 10:25:55 -0600

    check null values

--- edit_comment.php
+++ edit_comment.php
@@ -24,20 +24,26 @@ $comment_id = (int)$_POST['comment_id'];
 $comment = (string)$_POST['comment'];
 $story_id = (int)$_POST['story_id'];
 
-$stmt = $mysqli->prepare("UPDATE comments SET comment = ? WHERE id = ?");
-if(!$stmt){
-	printf("Query Prep Failed: %s\n", $mysqli->error);
-	exit;
+if($comment == ""){
+	echo "<p> Comment was not saved. Make sure to fill out the comment section! </p>";
 }
+else{
+	$stmt = $mysqli->prepare("UPDATE comments SET comment = ? WHERE id = ?");
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
 
-$stmt->bind_param('si', $comment, $comment_id);
+	$stmt->bind_param('si', $comment, $comment_id);
 
-$stmt->execute();
-$stmt->close();
+	$stmt->execute();
+	$stmt->close();
+	echo "<p> Success! Comment has been changed. </p>";
+}
 
 
 ?>
-<p> Success! Comment has been changed. Want to return back to story? <p>
+<p> Want to return back to story? <p>
 <form action ="view_story.php" method="POST">
 <input type="hidden" name="story_id" value="<?php printf($story_id); ?>"/>
 <input type="hidden" name="token" value="<?php echo $_SESSION['token'];?>" />
